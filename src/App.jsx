0db07efd38563ecda473dcfb8ad207223ebbddbc
black.dev.x <thiago.dev.x.@gmail.com>
2021-10-02 15:34:47 -0300

    flex on the cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import './App.css'
 import React from 'react'
 import First from './components/basics/First'
 import WithParameter from './components/basics/WithParameter'
@@ -6,20 +7,22 @@ import Random from './components/basics/Random'
 import Card from './components/layout/Card'
 
 const App = _ => (
-  <div>
+  <div className='App'>
     <h1>React Fundamentals</h1>
-    <Card title='#04 Random Number'>
-      <Random max={10} min={5}></Random>
-    </Card>
-    <Card title='#03 React Fragment'>
-      <Fragment />
-    </Card>
-    <Card title='#02 Component with props'>
-      <WithParameter title='second component' subtitle='with props'></WithParameter>
-    </Card>
-    <Card title='#01 First component'>
-      <First></First>
-    </Card>
+    <section className='exercises'>
+      <Card title='#04 Random Number'>
+        <Random max={10} min={5}></Random>
+      </Card>
+      <Card title='#03 React Fragment'>
+        <Fragment />
+      </Card>
+      <Card title='#02 Component with props'>
+        <WithParameter title='second component' subtitle='with props'></WithParameter>
+      </Card>
+      <Card title='#01 First component'>
+        <First></First>
+      </Card>
+    </section>
   </div>
 )
 
