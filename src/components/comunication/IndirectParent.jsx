a021a82c41b91108ee4be7183daf19487b6ee505
black.dev.x <thiago.dev.x.@gmail.com>
2021-10-09 23:55:11 -0300

    setState

--- src/components/comunication/IndirectParent.jsx
+++ src/components/comunication/IndirectParent.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import IndirectChild from './IndirectChild'
 
 const IndirectParent = () => {
+  const [information, setInformation] = useState('none')
   const receiveInformation = value => {
-    alert(`received ${value} from child`)
+    setInformation(value)
   }
   return (
     <div>
       <IndirectChild action={receiveInformation} />
+      <p>Latest information received: {information}</p>
     </div>
   )
 }
